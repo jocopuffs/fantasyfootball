4ba28b2900cf691cae1401994598aebc5c05fc88
Joe Collins <JoeCollins@Charles-7.local>
2019-12-04 17:42:10 +0800

    cool commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Client } from 'espn-fantasy-football-api';
+const myClient = new Client({ leagueId: 58950239});
+myClient.setCookies({ espnS2: 'AEAxAvlN7dGefUrKDnhqNIdJZaD8fTgUD8fQARKqAUoGwNk0Yo2%2BtiTnrES1HH0w6JQnpFI7b4%2FyfntfXJq2E8O9g84jIyhWZkvX1NFoSc%2BRdyts538tc76FoF4VeB1Qly9HUuyAw3%2BpJwJl0z8a0G3nNAoZrujDYphX6dqePwnjOcNtHTf%2BTOW7TsWapVQ4Q7AI%2FlArkQ%2FeWNBPTsqnEa6KnFsGrh5aKJSqACfI7sso9%2FJtRgbeDGE8NlYIu4ag4talHj1xXfUUhnxVhNECsPb4', SWID: '{048EDC5B-DBC9-4AF7-9A91-9DB57A99DFE2}' });
+
+
 
 function App() {
   return (
@@ -10,10 +15,17 @@ function App() {
          <div class="title">
            <p>fantasy football hub</p>
          </div>
+         <div class="add">
+           <p>add league</p>
+         </div>
        </div>
       </header>
     </div>
   );
+}
+
+function standPop() {
+  
 }
 
 export default App;
